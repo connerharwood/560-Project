52d2321e20e985691df06d892aa4a13f06faf11c
connerharwood <33815089+connerharwood@users.noreply.github.com>
2023-10-31 23:19:04 -0600

    rename

--- clean-data/code/waterUse_cleaning.R
+++ clean-data/code/waterUse_cleaning.R
@@ -1,18 +1,2 @@
 library(dplyr)
 library(tidyverse)
-
-load("~/560-Project/raw-data/data/rawData.rda")
-
-# 
-waterUse |> 
-  select(County) |> 
-  distinct()
-waterUse |> 
-  group_by(System.Name) |> 
-  filter(n() > 1)
-waterUse_counties = waterUse |> 
-  filter(County == c("Box Elder", "Cache", "Davis", "Morgan", "Rich", "Salt Lake", "Tooele", "Utah", "Weber"))
-
-waterUse_counties |> 
-  select(System.Name) |> 
-  n_distinct()
