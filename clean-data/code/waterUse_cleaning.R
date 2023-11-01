f8862488a77fdcc2f2db5f3b502ab78430d83510
connerharwood <33815089+connerharwood@users.noreply.github.com>
2023-11-01 00:38:57 -0600

    .

--- clean-data/code/waterUse_cleaning.R
+++ clean-data/code/waterUse_cleaning.R
@@ -11,19 +11,17 @@ load("~/560-Project/raw-data/data/waterUse_raw.rda")
 # Already done
 
 # 2: Import data and wrangle into a tidy layout
-
-# Use rbind to append waterUse_county dataframes
-waterUse_raw = rbind(waterUse_boxelder, waterUse_cache, waterUse_davis, waterUse_morgan,
-                     waterUse_rich, waterUse_saltlake, waterUse_tooele, waterUse_utah,
-                     waterUse_weber)
-
-# Use rbind to append waterUse_county_info dataframes
-waterUse_info = rbind(waterUse_boxelder_info, waterUse_cache_info, waterUse_davis_info, waterUse_morgan_info,
-                      waterUse_rich_info, waterUse_saltlake_info, waterUse_tooele_info, waterUse_utah_info,
-                      waterUse_weber_info)
+# Already done
 
 # 3: Remove irrelevant, garbage, or empty rows and columns
 
 # select relevant variables from waterUse_raw
 waterUse3 = waterUse_raw |> 
-  
+  select(
+    "System Name",
+    "System ID",
+    "System Type",
+    "History Year",
+    "County",
+    
+  )
