c773a716dd5d97adf554e685bef7ec995473668f
connerharwood <33815089+connerharwood@users.noreply.github.com>
2023-11-03 15:43:01 -0600

    .

--- clean-data/code/gsl_cleaning.R
+++ clean-data/code/gsl_cleaning.R
@@ -1,19 +1,52 @@
 library(dplyr)
 library(tidyr)
+library(skimr)
 
-GSL_northLevels = GSL_northLevels |> 
-  mutate(north_levels = X_62614_00003) |>
-  select(-X_62614_00003, 
-         -agency_cd,
-         -site_no,
-         -X_62614_00003_cd)
+load("~/560-Project/raw-data/data/rawData.rda")
 
-GSL_southLevels = GSL_southLevels |> 
-  mutate(south_levels = X_62614_00003) |>
-  select(-X_62614_00003,
-         -agency_cd,
-         -site_no,
-         -X_62614_00003_cd)
+# select and rename variables of interest for North Arm of GSL
+GSL_northLevels_clean = GSL_northLevels |> 
+  select(date = Date, north_levels = X_62614_00003) |> 
+  separate(date, into = c("year", "month", "day"))
+
+# select and rename variables of interest for South Arm of GSL
+GSL_southLevels_clean = GSL_southLevels |>
+  select(date = Date, south_levels = X_62614_00003) |> 
+  separate(date, into = c("year", "month", "day"))
+
+# check duplicates
+GSL_northLevels_clean |> 
+  n_distinct()
+
+# check duplicates
+GSL_southLevels_clean |> 
+  n_distinct()
+
+## No duplicates in either ##
+
+# check missing values in North Arm
+skim(GSL_northLevels_clean)
+
+# check missing values in South Arm
+skim(GSL_southLevels_clean)
+
+## No missing values in either ##
+
+## check for missing years ##
+all_years = 1966:2023
+
+# missing years in North Arm
+GSL_northLevels_years = unique(GSL_northLevels_clean$year)
+missing_GSL_northLevels_year = setdiff(all_years, GSL_northLevels_years)
+print(missing_GSL_northLevels_year)
+
+# missing years in South Arm
+GSL_southLevels_years = unique(GSL_southLevels_clean$year)
+missing_GSL_southLevels_year = setdiff(all_years, GSL_southLevels_years)
+print(missing_GSL_southLevels_year)
+
+## No missing years in either ##
+
 
 gsl_levels = merge(GSL_northLevels, GSL_southLevels, by = "Date")
 
