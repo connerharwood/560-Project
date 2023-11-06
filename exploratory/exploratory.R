f7491299f12015f7451ed470143bf7316949a307
connerharwood <33815089+connerharwood@users.noreply.github.com>
2023-11-05 20:52:48 -0700

    plots

--- exploratory/exploratory.R
+++ exploratory/exploratory.R
@@ -53,17 +53,17 @@ ggsave("gslLevels.png", plot = plot2)
 plot3 = ggplot(masterData, aes(x = year, y = perCapita_usage / 1000)) +
   geom_smooth(span = 0.01) +
   labs( 
-    title = "Water Usage Per Capita Over Time",
+    title = "Water Usage Per Capita",
     x = "Year", 
     y = "Water Usage Per Capita") + 
   theme_minimal() +
   theme(plot.title = element_text(hjust = 0.5))
 
 # save plot as .png file
-ggsave("")
+ggsave("perCapita_usage.png", plot = plot3)
 
 # create a scatterplot showing precipitation and water usage 
-ggplot(masterData, aes(x = precipitation, y = log(total_gallons), color = water_use)) + 
+plot4 = ggplot(masterData, aes(x = precipitation, y = log(total_gallons), color = water_use)) + 
   geom_smooth(span = 0.5, se = FALSE) + 
   labs( 
     title = "Yearly Total Precipitation vs. Log Yearly Water Usage",
@@ -71,30 +71,8 @@ ggplot(masterData, aes(x = precipitation, y = log(total_gallons), color = water_
     y = "Log Yearly Water Usage") + 
   theme_minimal()
 
-# normalize data to create different scales
-gsl_max = max(yearly_data$gsl_level)
-usage_max = max(yearly_data$ln_total)
-gsl_min = min(yearly_data$gsl_level)
-
-yearly_data$normalized_gsl <- (yearly_data$gsl_level - gsl_min) / (gsl_max - gsl_min)
-yearly_data$normalized_usage <- yearly_data$ln_total / usage_max
-
-# create a graph showing water use totals relative to GSL water levels 
-ggplot() +
-  geom_smooth(data = yearly_data, aes(x = year, y = normalized_gsl, color = "GSL Level")) +
-  geom_smooth(data = yearly_data, aes(x = year, y = normalized_usage, color = "Log Yearly Water Use Total")) +
-  geom_smooth(method = "lm", se = FALSE) +
-  scale_y_continuous(
-    sec.axis = sec_axis(~ . * usage_max, name = "Log Yearly Water Use Total"),
-    name = "GSL Level"
-  ) +
-  labs(
-    title = "GSL Water Levels and Water Use Totals Over Time",
-    x = "Year"
-  ) +
-  theme_minimal() +
-  theme(legend.position = "bottomright") +
-  scale_color_manual(values = c("GSL Level" = "green", "Log Yearly Water Use Total" = "red"))
+# save plot as .png file
+ggsave("precip_waterUsage.png", plot = plot4)
 
 #------------------------------------------------------------------------------#
 ## Create separate dataset to focus on water-related land use data ##
@@ -116,7 +94,7 @@ agData = agData2 |>
   mutate(yearly_usage = sum(total_gallons))
 
 # graph yearly water usage and water-related land usage per year in the agricultural sector
-ggplot() +
+plot5 = ggplot() +
   geom_smooth(data = agData, aes(x = year, y = yearly_acres, color = "Ag Land Use"), se = FALSE) +
   geom_smooth(data = agData, aes(x = year, y = yearly_usage / 1000000, color = "Ag Water Usage"), se = FALSE) +
   scale_y_continuous(
@@ -128,3 +106,6 @@ ggplot() +
     title = "Agricultural Water Use and Water-Related Land Use"
   ) +
   theme_minimal()
+
+# save plot as .png file
+ggsave("agPlots.png", plot = plot5)
