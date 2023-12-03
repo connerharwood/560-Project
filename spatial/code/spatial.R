757f8457084a5c159eb305e3add838ab33355167
connerharwood <33815089+connerharwood@users.noreply.github.com>
2023-12-03 16:17:05 -0700

    .

--- spatial/code/spatial.R
+++ spatial/code/spatial.R
@@ -86,25 +86,23 @@ basin_plot = ggplot() +
   theme_void() +
   labs(fill = "", title = "Great Salt Lake Basin") +
   theme(
-    legend.position = c(1.022, 0.99),
+    legend.position = c(0.945, .98),
     legend.justification = c(1, 1),
     legend.margin = margin(t = 0, r = 10, b = 0, l = 0),
-    legend.key.size = unit(0.43, "cm"),
-    legend.text = element_text(size = 7.3),
+    legend.key.size = unit(0.65, "cm"),
+    legend.text = element_text(size = 8.5),
     plot.title = element_text(hjust = 0.5, size = 15, margin = margin(b = -15))
   )
 
 # original position for legend: c(1.023, 0.99)
 
 # show plot
-print(basin_plot)
+#print(basin_plot)
 
 # save in higher resolution, maintain legend position
 ggsave(
   filename = "basin_plot.png",
   plot = basin_plot,
-  #width = 5, 
-  #height = 3, 
   units = "in", 
   dpi = 300,
 )
