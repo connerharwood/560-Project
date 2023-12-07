ec4d911c5c4a8faa0a4aba705aab33155c812762
connerharwood <33815089+connerharwood@users.noreply.github.com>
2023-12-06 21:36:52 -0700

    Ran regressions

--- econometric-analysis/code/regression_data.R
+++ econometric-analysis/code/regression_data.R
@@ -10,14 +10,14 @@ load("~/560-Project/clean-data/data/masterdata.rds")
 # Aggregate monthly data for 1996-2014 ----
 
 # convert month and year to date format
-masterdata_monthly = masterdata |> 
+masterdata2014 = masterdata |> 
   mutate(
     date = as.Date(paste(year, month, "01"), format = "%Y %b %d"),
     date = as.yearmon(date)
   )
 
 # GSL monthly level and volume
-gsl_monthly = masterdata_monthly |> 
+gsl_monthly2014 = masterdata2014 |> 
   # convert volume from cubic meters to gallons
   mutate(
     gsl_volume_gal = gsl_volume_m3 * 264.172052
@@ -38,7 +38,7 @@ gsl_monthly = masterdata_monthly |>
   filter(date >= 1997)
 
 # yearly total population across GSL Basin counties, assigned to each month for the regression
-pop_monthly1 = masterdata_monthly |> 
+pop_monthly2014 = masterdata_monthly |> 
   select(date, county, population_thousands) |> 
   # convert population in thousands to true population
   mutate(population = population_thousands * 1000) |> 
@@ -55,7 +55,7 @@ pop_monthly1 = masterdata_monthly |>
     month = as.integer(format(date, "%m"))
   )
 
-pop_monthly_impute = pop_monthly1 |> 
+pop_monthly_impute2014 = pop_monthly2014 |> 
   mutate(
     year = as.integer(format(date, "%Y")),
     month = as.integer(format(date, "%m"))
@@ -68,14 +68,14 @@ pop_monthly_impute = pop_monthly1 |>
   )
 
 # merge
-pop_monthly_imputed = left_join(pop_monthly1, pop_monthly_impute, by = "year", relationship = "many-to-one")
+pop_monthly_imputed2014 = left_join(pop_monthly2014, pop_monthly_impute2014, by = "year", relationship = "many-to-one")
 
-pop_monthly = pop_monthly_imputed |> 
+pop_monthly2014 = pop_monthly_imputed2014 |> 
   select(date, population = population.x, pop_change) |> 
   filter(date >= 1997)
   
 # monthly average precipitation across GSL Basin counties
-precip_monthly = masterdata_monthly |> 
+precip_monthly2014 = masterdata2014 |> 
   select(date, county, precip_in) |> 
   group_by(date, county) |> 
   summarize(
@@ -91,7 +91,7 @@ precip_monthly = masterdata_monthly |>
   filter(date >= 1997)
 
 # monthly total water usage per use type
-wateruse_monthly1 = masterdata_monthly |>
+wateruse_monthly2014 = masterdata2014 |>
   filter(month_gallons != 0) |> 
   select(date, use_type, month_gallons) |> 
   group_by(date, use_type) |> 
@@ -100,11 +100,11 @@ wateruse_monthly1 = masterdata_monthly |>
   )
 
 # look at distributuon of monthly gallons per use type to see if we should log transform
-ggplot(wateruse_monthly1, aes(x = month_gallons, color = use_type, fill = use_type)) +
+ggplot(wateruse_monthly2014, aes(x = month_gallons, color = use_type, fill = use_type)) +
   geom_histogram(bins = 50) +
   facet_wrap(~use_type)
 
-wateruse_monthly2 = wateruse_monthly1 |> 
+wateruse_monthly2014_2 = wateruse_monthly2014 |> 
   # log transform monthly gallons
   mutate(log_month_gallons = log(month_gallons)) |> 
   group_by(use_type) |> 
@@ -118,18 +118,18 @@ wateruse_monthly2 = wateruse_monthly1 |>
   mutate(log_month_gallons_change = ifelse(is.na(log_month_gallons_change), 0, log_month_gallons_change))
 
 # check distribution of log transformed water use
-ggplot(wateruse_monthly2, aes(x = log_month_gallons_change, color = use_type, fill = use_type)) +
+ggplot(wateruse_monthly2014_2, aes(x = log_month_gallons_change, color = use_type, fill = use_type)) +
   geom_histogram() +
   facet_wrap(~use_type)
 
 # merge monthly datasets into one
-monthly_merge1 = left_join(wateruse_monthly2, gsl_monthly, by = "date", relationship = "many-to-one")
-monthly_merge2 = left_join(monthly_merge1, pop_monthly, by = "date", relationship = "many-to-one")
-monthly_merge3 = left_join(monthly_merge2, precip_monthly, by = "date", relationship = "many-to-one")
+monthly2014_merge1 = left_join(wateruse_monthly2014_2, gsl_monthly2014, by = "date", relationship = "many-to-one")
+monthly2014_merge2 = left_join(monthly2014_merge1, pop_monthly2014, by = "date", relationship = "many-to-one")
+monthly2014_merge3 = left_join(monthly2014_merge2, precip_monthly2014, by = "date", relationship = "many-to-one")
 
-data2014 = monthly_merge3
+total_data2014 = monthly2014_merge3
 
-save(data2014, file = "total_data2014.rds")
+save(total_data2014, file = "total_data2014.rds")
 
 #------------------------------------------------------------------------------#
 # 1996-2022 ----
@@ -167,15 +167,92 @@ merge4 = merge3 |>
 
 masterdata2022_1 = merge4
 
-#------------------------------------------------------------------------------#
-# Aggregate 1996-2022 monthly data ----
-
 # convert month and year to date format
 masterdata2022_2 = masterdata2022_1 |> 
   mutate(
     date = as.Date(paste(year, month, "01"), format = "%Y %b %d"),
     date = as.yearmon(date)
   )
+
+#------------------------------------------------------------------------------#
+# GSL, population, and precipitation for 1996-2022 ----
+
+# GSL monthly level and volume
+gsl_monthly2022 = masterdata2022_2 |> 
+  # convert volume from cubic meters to gallons
+  mutate(
+    gsl_volume_gal = gsl_volume_m3 * 264.172052
+  ) |> 
+  select(date, gsl_level_ft, gsl_volume_gal) |> 
+  group_by(date) |> 
+  summarize(
+    gsl_level_ft = mean(gsl_level_ft),
+    gsl_volume_gal = mean(gsl_volume_gal)
+  ) |> 
+  arrange(date) |> 
+  # calculate year-over-year change in level and volume for each month
+  mutate(
+    gsl_level_change = gsl_level_ft - lag(gsl_level_ft, 12),
+    gsl_volume_change = gsl_volume_gal - lag(gsl_volume_gal, 12)
+  ) |> 
+  # filter out 1996, which has NAs for change variables
+  filter(date >= 1997)
+
+# yearly total population across GSL Basin counties, assigned to each month for the regression
+pop_monthly2022 = masterdata2022_2 |> 
+  select(date, county, population_thousands) |> 
+  # convert population in thousands to true population
+  mutate(population = population_thousands * 1000) |> 
+  group_by(date, county) |> 
+  summarize(
+    population = mean(population)
+  ) |> 
+  group_by(date) |> 
+  summarize(
+    population = sum(population)
+  ) |> 
+  mutate(
+    year = as.integer(format(date, "%Y")),
+    month = as.integer(format(date, "%m"))
+  )
+
+pop_monthly_impute2022 = pop_monthly2022 |> 
+  mutate(
+    year = as.integer(format(date, "%Y")),
+    month = as.integer(format(date, "%m"))
+  ) |> 
+  group_by(year) |> 
+  summarize(population = mean(population)) |> 
+  mutate(
+    # compute average monthly population change based on year-to-year population change
+    pop_change = (population - lag(population)) / 12
+  )
+
+# merge
+pop_monthly_imputed2022 = left_join(pop_monthly2022, pop_monthly_impute2022, by = "year", relationship = "many-to-one")
+
+pop_monthly2022 = pop_monthly_imputed2022 |> 
+  select(date, population = population.x, pop_change) |> 
+  filter(date >= 1997)
+
+# monthly average precipitation across GSL Basin counties
+precip_monthly2022 = masterdata2022_2 |> 
+  select(date, county, precip_in) |> 
+  group_by(date, county) |> 
+  summarize(
+    precip_in = mean(precip_in)
+  ) |> 
+  group_by(date) |> 
+  summarize(
+    precip_in = mean(precip_in)
+  ) |>
+  # calculate year-over-year change in precipitation for each month
+  mutate(precip_change = precip_in - lag(precip_in, 12)) |> 
+  # filter out 1996, which has NAs for precip_change
+  filter(date >= 1997)
+
+#------------------------------------------------------------------------------#
+# Aggregate 1996-2022 monthly data ----
 
 # monthly total water usage per use type
 wateruse2022_1 = masterdata2022_2 |>
@@ -200,19 +277,19 @@ wateruse2022_2 = wateruse2022_1 |>
   mutate(log_month_gallons_change = ifelse(is.na(log_month_gallons_change), 0, log_month_gallons_change))
 
 # merge monthly datasets into one
-monthly2022_merge1 = left_join(wateruse2022_2, gsl_monthly, by = "date", relationship = "many-to-one")
-monthly2022_merge2 = left_join(monthly2022_merge1, pop_monthly, by = "date", relationship = "many-to-one")
-monthly2022_merge3 = left_join(monthly2022_merge2, precip_monthly, by = "date", relationship = "many-to-one")
+monthly2022_merge1 = left_join(wateruse2022_2, gsl_monthly2022, by = "date", relationship = "many-to-one")
+monthly2022_merge2 = left_join(monthly2022_merge1, pop_monthly2022, by = "date", relationship = "many-to-one")
+monthly2022_merge3 = left_join(monthly2022_merge2, precip_monthly2022, by = "date", relationship = "many-to-one")
 
-data2022 = monthly2022_merge3
+total_data2022 = monthly2022_merge3
 
-save(data2022, file = "total_data2022.rds")
+save(total_data2022, file = "total_data2022.rds")
 
 #------------------------------------------------------------------------------#
 # Create variables for total use by each use type for 1996-2014 ---- 
 
 # assign change in monthly gallons for each use type to new use type variable
-reg_data2014_1 = data2014 |> 
+reg_data2014_1 = total_data2014 |> 
   group_by(use_type) |>
   mutate(value = log_month_gallons) |>
   ungroup() |>
@@ -240,7 +317,7 @@ save(reg_data2014, file = "reg_data2014.rds")
 # Create variables for total use by each use type for 1996-2022 ---- 
 
 # assign change in monthly gallons for each use type to new use type variable
-reg_data2022_1 = data2022 |> 
+reg_data2022_1 = total_data2022 |> 
   group_by(use_type) |>
   mutate(value = log_month_gallons) |>
   ungroup() |>
